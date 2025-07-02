a80e74be91c0ee555fb788a3ddaafef8f98dfc10
김주영 <98079486+Leusin@users.noreply.github.com>
2025-07-02 10:27:44 +0900

    [추가] 게임모드와 로그인 실습 예제

--- UnrealNetworkingMultiplayer/Source/ArenaBattle/ArenaBattle.h
+++ UnrealNetworkingMultiplayer/Source/ArenaBattle/ArenaBattle.h
@@ -4,3 +4,10 @@
 
 #include "CoreMinimal.h"
 
+#define LOG_NETMODEINFO ((GetNetMode() == ENetMode::NM_Client) ? TEXT("Client") : ((GetNetMode() == ENetMode::NM_Standalone) ? TEXT("StandAlone") : TEXT("Server")))
+#define LOG_CALLINFO ANSI_TO_TCHAR(__FUNCTION__) // 매크로가 호출되고 있는 함수 이름 가져옴
+// UE_LOG 매크로를 대신함
+#define AB_LOG(LogCat, Verbosity, Format, ...) UE_LOG(LogCat, Verbosity, TEXT("[%s] %s %s"), LOG_NETMODEINFO, LOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
+
+
+DECLARE_LOG_CATEGORY_EXTERN(LogABNetwork, Log, All);
