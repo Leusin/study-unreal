0f3f8a26606064850e16b350ce633bb54f01e2f6
김주영 <98079486+Leusin@users.noreply.github.com>
2025-07-09 13:21:35 +0900

    [추가] 액터 리플리케이션 기초

--- UnrealNetworkingMultiplayer/Source/ArenaBattle/ArenaBattle.h
+++ UnrealNetworkingMultiplayer/Source/ArenaBattle/ArenaBattle.h
@@ -4,10 +4,12 @@
 
 #include "CoreMinimal.h"
 
-#define LOG_NETMODEINFO ((GetNetMode() == ENetMode::NM_Client) ? TEXT("Client") : ((GetNetMode() == ENetMode::NM_Standalone) ? TEXT("StandAlone") : TEXT("Server")))
+#define LOG_LOCALROLEINFO *(UEnum::GetValueAsString(TEXT("Engine.ENetRole"), GetLocalRole()))
+#define LOG_REMOTEROLEINFO *(UEnum::GetValueAsString(TEXT("Engine.ENetRole"), GetRemoteRole()))
+#define LOG_NETMODEINFO ((GetNetMode() == ENetMode::NM_Client) ? *FString::Printf(TEXT("CLIENT%d"), UE::GetPlayInEditorID()) : ((GetNetMode() == ENetMode::NM_Standalone) ? TEXT("STANDALONE") : TEXT("SERVER")))
 #define LOG_CALLINFO ANSI_TO_TCHAR(__FUNCTION__) // 매크로가 호출되고 있는 함수 이름 가져옴
 // UE_LOG 매크로를 대신함
-#define AB_LOG(LogCat, Verbosity, Format, ...) UE_LOG(LogCat, Verbosity, TEXT("[%s] %s %s"), LOG_NETMODEINFO, LOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
+#define AB_LOG(LogCat, Verbosity, Format, ...) UE_LOG(LogCat, Verbosity, TEXT("[%s][%s/%s] %s %s"), LOG_NETMODEINFO, LOG_LOCALROLEINFO, LOG_REMOTEROLEINFO, LOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
 
 
-DECLARE_LOG_CATEGORY_EXTERN(LogABNetwork, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogABNetwork, Log, All); 
--- UnrealNetworkingMultiplayer/Source/ArenaBattle/Prop/ABFountain.cpp
+++ UnrealNetworkingMultiplayer/Source/ArenaBattle/Prop/ABFountain.cpp
@@ -3,6 +3,8 @@
 
 #include "Prop/ABFountain.h"
 #include "Components/StaticMeshComponent.h"
+#include "Net/UnrealNetwork.h"
+#include "ArenaBattle.h"
 
 // Sets default values
 AABFountain::AABFountain()
@@ -28,6 +30,8 @@ AABFountain::AABFountain()
 	{
 		Water->SetStaticMesh(WaterMeshRef.Object);
 	}
+
+	bReplicates = true;
 }
 
 // Called when the game starts or when spawned
@@ -35,6 +39,16 @@ void AABFountain::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	// 값이 변경 될 때만 클라이언트에 전송 
+	/*if (HasAuthority())
+	{
+		FTimerHandle Handle;
+		GetWorld()->GetTimerManager().SetTimer(Handle, FTimerDelegate::CreateLambda([&]
+			{
+				ServerRotationYaw += 1.f;
+			}
+		), 1.f, true, 0.f);
+	}*/
 }
 
 // Called every frame
@@ -42,5 +56,44 @@ void AABFountain::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (HasAuthority())
+	{
+		AddActorLocalRotation(FRotator(0.0f, RotationRate * DeltaTime, 0.0f));
+		ServerRotationYaw = RootComponent->GetComponentRotation().Yaw;
+	}
+	else
+	{
+		// 매 프레임 마다 서버로부터 받은 속성 업데이트
+		// 만일 상대방의 네트워크 전송 주기가 틱보다 느린 경우 데이터를 받기 전에 틱이 여러번 호출 되어 있어, 
+		// 비효율적으로 동작할 수 있음. 이 경우에는 콜백함수를 등록하는 편이 나음 
+		/*FRotator NewRotator = RootComponent->GetComponentRotation();
+		NewRotator.Yaw = ServerRotationYaw;
+		RootComponent->SetWorldRotation(NewRotator);*/
+	}
 }
 
+void AABFountain::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AABFountain, ServerRotationYaw);
+}
+
+// 리플리케이션은 액터 채널을 통해 작업이 된다. 
+void AABFountain::OnActorChannelOpen(FInBunch& InBunch, UNetConnection* Connection)
+{
+	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("Begin"));
+
+	Super::OnActorChannelOpen(InBunch, Connection);
+
+	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("End"));
+}
+
+void AABFountain::OnRep_ServerRotationYaw()
+{
+	AB_LOG(LogABNetwork, Log, TEXT("Yaw : %f"), ServerRotationYaw);
+
+	FRotator NewRotator = RootComponent->GetComponentRotation();
+	NewRotator.Yaw = ServerRotationYaw;
+	RootComponent->SetWorldRotation(NewRotator);
+}
